35debe83bac9380f774b69d47f6469545cdce6ca
Ambuj Nayak <107915051+A-nayak@users.noreply.github.com>
2025-10-07 18:15:34 +0530

    Update 64._Minimum_Path_Sum.cpp

--- DP/A2Z/CodingNinjas/64._Minimum_Path_Sum.cpp
+++ DP/A2Z/CodingNinjas/64._Minimum_Path_Sum.cpp
@@ -46,3 +46,27 @@ public:
         return helper(g,m,n,0,0);
     }
 };
+
+
+// bottom up
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& g) {
+        int m=g.size();
+        int n=g[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=g[0][0];
+        for(int i=1;i<m;i++){
+            dp[i][0]=g[i][0]+dp[i-1][0];
+        }
+        for(int j=1;j<n;j++){
+            dp[0][j]=g[0][j]+dp[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=min(g[i][j]+dp[i-1][j],g[i][j]+dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
