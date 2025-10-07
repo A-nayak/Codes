882599417994de215de606a9119955396b6f25a1
Ambuj Nayak <107915051+A-nayak@users.noreply.github.com>
2025-10-07 15:17:35 +0530

    Create 62._Unique_Paths.cpp

---
+++ DP/A2Z/CodingNinjas/62._Unique_Paths.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/unique-paths/
+// pure recursion
+
+class Solution {
+public:
+    int helper(int m,int n,int i,int j){
+        if(i>=m || j>=n)    //out of bound case so path is invalid return 0
+        return 0;
+        if(i==m-1 && j==n-1)   // we reached destination mark path as valid and count 1 valid path
+        return 1;
+        int right=helper(m,n,i,j+1);   // move right check if we can reach destination moving right
+        int down=helper(m,n,i+1,j); // same as above but for down
+        return right+down;   // total ways to reach destination moving right and down
+    }
+    int uniquePaths(int m, int n) {
+        return helper(m,n,0,0);
+    }
+};
+
