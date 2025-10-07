f5314c2290c8e6e6f3fa070c377d4a2bc3c9d862
Ambuj Nayak <107915051+A-nayak@users.noreply.github.com>
2025-10-07 15:21:53 +0530

    Update 62._Unique_Paths.cpp

--- DP/A2Z/CodingNinjas/62._Unique_Paths.cpp
+++ DP/A2Z/CodingNinjas/62._Unique_Paths.cpp
@@ -17,3 +17,24 @@ public:
     }
 };
 
+
+// rec + memo
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int helper(int m,int n,int i,int j){
+        if(i>=m || j>=n)
+        return 0;
+        if(i==m-1 && j==n-1)
+        return 1;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int right=helper(m,n,i,j+1);
+        int down=helper(m,n,i+1,j);
+        return dp[i][j]=right+down;
+    }
+    int uniquePaths(int m, int n) {
+        dp.resize(m,vector<int>(n,-1));
+        return helper(m,n,0,0);
+    }
+};
