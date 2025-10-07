65b4225b29d8eff415e47f75c9ecdafa0c28e6b3
Ambuj Nayak <107915051+A-nayak@users.noreply.github.com>
2025-10-07 15:49:59 +0530

    Update 62._Unique_Paths.cpp

--- DP/A2Z/CodingNinjas/62._Unique_Paths.cpp
+++ DP/A2Z/CodingNinjas/62._Unique_Paths.cpp
@@ -46,18 +46,18 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        dp[0][0]=1;
-        for(int i=0;i<n;i++){
+        dp[0][0]=1;          // if there is only one block then reaching it have only 1 uniq path
+        for(int i=0;i<n;i++){       // if only one row exist we can reach each block just by moving right so 1 uniq path
             dp[0][i]=1;
         }
-        for(int j=0;j<m;j++){
+        for(int j=0;j<m;j++){       // if only one column exist we can reach each blcok by moving down so 1 uniq path
             dp[j][0]=1;
         }
-        for(int i=1;i<m;i++){
+        for(int i=1;i<m;i++){        
             for(int j=1;j<n;j++){
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m-1][n-1];        // if m and n block we have we return this value 
     }
 };
