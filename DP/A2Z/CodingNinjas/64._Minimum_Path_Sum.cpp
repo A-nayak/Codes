833face2248fd5253069c3821d926f3c35b876f3
Ambuj Nayak <107915051+A-nayak@users.noreply.github.com>
2025-10-07 18:07:49 +0530

    Update 64._Minimum_Path_Sum.cpp

--- DP/A2Z/CodingNinjas/64._Minimum_Path_Sum.cpp
+++ DP/A2Z/CodingNinjas/64._Minimum_Path_Sum.cpp
@@ -21,3 +21,28 @@ public:
         return helper(g,m,n,0,0);
     }
 };
+
+// recursion +memo
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int helper(vector<vector<int>>& g, int m,int n,int i,int j){
+        if(i>=m || j>=n){
+            return INT_MAX;              
+        }
+        if(i==m-1 && j==n-1){
+            return g[i][j];
+        }
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        long long right=g[i][j]+1ll*helper(g,m,n,i+1,j);
+        long long bottom=g[i][j]+1ll*helper(g,m,n,i,j+1);
+        return dp[i][j]=min(right,bottom);
+    }
+    int minPathSum(vector<vector<int>>& g) {
+        int m=g.size();
+        int n=g[0].size();
+        dp.resize(m,vector<int>(n,-1));
+        return helper(g,m,n,0,0);
+    }
+};
